72de9183233e44e3b2772067fe55a7f06ccc23cb
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-09-05 13:57:00 +0200

    input fixed

--- main_dino.py
+++ main_dino.py
@@ -127,6 +127,7 @@ def get_args_parser():
     parser.add_argument("--dist_url", default="env://", type=str, help="""url used to set up
         distributed training; see https://pytorch.org/docs/stable/distributed.html""")
     parser.add_argument("--local_rank", default=0, type=int, help="Please ignore and do not set this argument.")
+    parser.add_argument("--local-rank", default=0, type=int, help="Please ignore and do not set this argument.")  # alias, same dest
     return parser
 
 import copy
